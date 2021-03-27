ad7a04139a1d122ca290ba2a77371437c4f3c0f1
whatever37 <martin.piko@protonmail.com>
2021-03-27 15:49:40 +0100

    PagesController

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'LSAPP2')}}</title>      
+    </head>
+    <body>
+        <h1>Welcome to Laravel</h1>
+        <p>This is laravel app from tutorial.</p>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
