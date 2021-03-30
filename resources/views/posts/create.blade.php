8c60cc53a2f7cbfcd68e1fa79610427ca2ddc5cc
whatever37 <martin.piko@protonmail.com>
2021-03-30 22:46:20 +0200

    laravelCollective form

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+ 
+@section('content')
+    <h1>nic</h1>
+    <h1>nic</h1>
+    <h1>Create Post</h1>
+
+    {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+    <div class="form-group">
+        {{Form::label('title', 'Title')}}
+        {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+
+    </div>
+    {!! Form::close() !!}
+
+
+
+@endsection
